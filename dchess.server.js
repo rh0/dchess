b1ef2883ca7d36ed255d5180d3f1f96eb9c9e92a
Ryan Oles <ryan@theoleschool.com>
2012-06-26 23:15:36 -0500

    very simple redis test to be sure that the DB persists across clients.  It does.

--- dchess.server.js
+++ dchess.server.js
@@ -9,29 +9,46 @@
  */
 
 var publishMessageToClient,
+    publishMessageToChannel,
     gameChannel = 'global_chess_channel',
-    publishMessageToChannel;
+    gameCount = 0,
+    redis = require("redis"),
+    gameBank = redis.createClient();
+
+gameBank.on("error", function (err) {
+  console.log("Redis Error! " + err);
+});
+
+gameBank.hmset("game", "channel", gameChannel);
 
 exports.setup = function (config) {
   publishMessageToClient = config.publishMessageToClient;
   publishMessageToChannel = config.publishMessageToChannel;
 
   process.on('client-connection', function (sessionId) {
-    console.log('Example extension got connection event for session ' + sessionId);
+    console.log('Got connection event for session ' + sessionId);
     //publishMessageToClient(sessionId, {data: {subject: 'Example extension', body: 'Hello, you just connected.'}});
   })
   .on('client-authenticated', function (sessionId, authData) {
-    console.log('Example extension got authenticated event for session ' + sessionId + ' (user ' + authData.uid + ')');
+    console.log('Got authenticated event for session ' + sessionId + ' (user ' + authData.uid + ')');
     //publishMessageToClient(sessionId, {data: {subject: 'Example extension', body: 'Welcome, you are authenticated.'}});
     config.addClientToChannel(sessionId, gameChannel);
+    console.log(gameBank.ping());
+    gameBank.hvals("game", function(err, reply) {
+      console.log(reply);
+    });
+    gameBank.hset("game", "player1", authData.uid);
+    gameBank.hvals("game", function(err, reply) {
+      console.log(reply);
+    });
   })
   .on('client-message', function (sessionId, message) {
-    console.log('Example extension got message event for session ' + sessionId + ': Fen -> ' + message.moveFen);
+    console.log('Got message event for session ' + sessionId + ': Fen -> ' + message.moveFen);
     handleMessage(sessionId, message);
 //    publishMessageToClient(sessionId, {data: {subject: 'Example extension', body: 'You sent the message: ' + require('util').inspect(message)}});
   })
   .on('client-disconnect', function (sessionId) {
-    console.log('Example extension got disconnect event for session ' + sessionId);
+    console.log('Got disconnect event for session ' + sessionId);
   });
 };
 
